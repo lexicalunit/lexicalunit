ae424b06819847b6592b9bd30300808d2553c6f6
lexicalunit <lexicalunit@lexicalunit.com>
2019-07-09 12:17:48 -0700

    Adds english towns.

--- README.md
+++ README.md
@@ -1,13 +1,5 @@
 # Publishing
 
 ```shell
-$ fab publish
-```
-
-**Requires `publish.yaml` to exist and provide the following configuration items.** For example:
-
-```yaml
-mainwebsite_html: /var/www/html
-user: me@lexicalunit.com
-hosts: [lexicalunit.com]
+$ ./publish.sh
 ```
---
+++ publish.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eu
+
+wd="$(mktemp -d)"
+trap 'rm -rf $wd' EXIT
+
+git archive master | tar -x -C "$wd"
+
+cd "$wd"
+rsync -av --chmod=a+r --rsh=ssh darwi "$PUSER@$PHOST:$PDEST"
+rsync -av --chmod=a+r --rsh=ssh github "$PUSER@$PHOST:$PDEST"
+rsync -av --chmod=a+r --rsh=ssh nanodbc "$PUSER@$PHOST:$PDEST"
+rsync -av --chmod=a+r --rsh=ssh pancake "$PUSER@$PHOST:$PDEST"
+rsync -av --chmod=a+r --rsh=ssh resume "$PUSER@$PHOST:$PDEST"
+rsync -av --chmod=a+r --rsh=ssh www "$PUSER@$PHOST:$PDEST"
+rsync -av --chmod=a+r --rsh=ssh .htaccess "$PUSER@$PHOST:$PDEST"
+rsync -av --chmod=a+r --rsh=ssh index.html "$PUSER@$PHOST:$PDEST"
