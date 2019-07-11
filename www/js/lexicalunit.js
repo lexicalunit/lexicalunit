0af7b89a0dab6723a43410cd4bc559689a851a4e
lexicalunit <lexicalunit@lexicalunit.com>
2019-07-11 10:30:36 -0700

    Better image swap with transition.

--- www/js/lexicalunit.js
+++ www/js/lexicalunit.js
@@ -1,30 +1,23 @@
-var show_top = false;
+var show_top = false
 function toggle() {
-    show_top = !show_top;
-    if(show_top) {
-        $('.top').addClass('invis');
-        $('.bottom').removeClass('invis');
-        $('.dryguy').attr('src', 'www/img/dryguy-stare.png');
-        $('.content').addClass('colorful');
-    } else {
-        $('.top').removeClass('invis');
-        $('.bottom').addClass('invis');
-        $('.dryguy').attr('src', 'www/img/dryguy.gif');
-        $('.content').removeClass('colorful');
-    }
+  show_top = !show_top
+  if(show_top) {
+    $('.top').addClass('invis')
+    $('.bottom').removeClass('invis')
+    $('.dryguy').attr('src', 'www/img/dryguy-stare.png')
+    $('.content').addClass('colorful')
+  } else {
+    $('.top').removeClass('invis')
+    $('.bottom').addClass('invis')
+    $('.dryguy').attr('src', 'www/img/dryguy.gif')
+    $('.content').removeClass('colorful')
+  }
 }
 
-String.prototype.splice = function(idx, rem, s) {
-    return (this.slice(0, idx) + s + this.slice(idx + Math.abs(rem)));
-};
-
 function hover(element) {
-    var img = element.getElementsByTagName('img')[0];
-    img.src = img.src.splice(img.src.lastIndexOf('/') + 1, 0, 'v');
+  element.classList.add('hovered')
 }
 
 function unhover(element) {
-    var img = element.getElementsByTagName('img')[0];
-    var pos = img.src.lastIndexOf('/');
-    img.src = img.src.slice(0, pos + 1) + img.src.slice(pos + 2);
+  element.classList.remove('hovered')
 }
