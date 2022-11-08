be00daa0a7d0c7bf69ceb76017d077c0e6727576
lexicalunit <lexicalunit@lexicalunit.com>
2022-11-08 10:02:04 -0800

    Update resume

--- resume/build.py
+++ resume/build.py
@@ -9,26 +9,31 @@ from subprocess import PIPE, STDOUT, Popen
 
 import yaml
 
-if any(opt in sys.argv for opt in ('-h', '--help')):
-    print('usage: ./build.py [--real]')
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+
+if any(opt in sys.argv for opt in ("-h", "--help")):
+    print("usage: ./build.py [--real]")
     sys.exit(0)
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger()
 
-BUILD_REAL = '--real' in sys.argv
-BUILD_DIGITAL = '--digital' in sys.argv
-CONF_FILE = 'resume.yaml'
+BUILD_REAL = "--real" in sys.argv
+BUILD_DIGITAL = "--digital" in sys.argv
+CONF_FILE = "resume.yaml"
 
 try:
-    conf = yaml.load(open(os.path.join(os.curdir, CONF_FILE), 'rb'))
+    conf = yaml.load(open(os.path.join(os.curdir, CONF_FILE), "rb"), Loader=Loader)
 except:
-    log.exception('error: unable to read {} config file:'.format(CONF_FILE))
+    log.exception("error: unable to read {} config file:".format(CONF_FILE))
     raise
 
 # real header has my real name and address, etc...
 if BUILD_REAL:
-    REAL_HEADER = conf['real_header']
+    REAL_HEADER = conf["real_header"]
 
 # anonymized header
 ANON_HEADER = r"""
@@ -61,22 +66,26 @@ with open("resume.tex", "r") as f:
         data = data.replace("% <Digital>", DIGITAL_PREAMBLE)
 
     if not BUILD_REAL:
-        lines = data.split('\n')
+        lines = data.split("\n")
         for lineno, line in enumerate(lines):
-            if line.startswith('\date'):
-                lines[lineno] = r'\dates{}'
-            elif line.startswith('\location'):
-                lines[lineno] = r'\location{}'
-            elif 'University' in line:
-                lines[lineno] = r'\employer{\textbf{School}}'
-            elif line.startswith('\employer'):
-                lines[lineno] = r'\employer{\textbf{Workplace}}'
-        data = '\n'.join(lines)
+            if line.startswith("\date"):
+                lines[lineno] = r"\dates{}"
+            elif line.startswith("\location"):
+                lines[lineno] = r"\location{}"
+            elif "University" in line:
+                lines[lineno] = r"\employer{\textbf{School}}"
+            elif line.startswith("\employer"):
+                lines[lineno] = r"\employer{\textbf{Workplace}}"
+        data = "\n".join(lines)
 print(data, file=open("built.tex", "w"))
 
 # run LaTeX processing
-p = Popen(['/Library/TeX/texbin/pdflatex', '--file-line-error' '--synctex=1'],
-          stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+p = Popen(
+    ["/Library/TeX/texbin/pdflatex", "--file-line-error" "--synctex=1"],
+    stdout=PIPE,
+    stdin=PIPE,
+    stderr=STDOUT,
+)
 out, err = p.communicate(input=data.encode("utf-8"))
 
 # show LaTeX processing output
@@ -85,14 +94,16 @@ if err:
     print(err, file=sys.stderr)
 
 # sanity check
-assert os.path.isfile('texput.pdf')
+assert os.path.isfile("texput.pdf")
 
 # cleanup
-os.remove('texput.log')
+os.remove("texput.log")
 
 # output
-OUTPUT = os.path.expanduser(conf['real_resume_install_path'] if BUILD_REAL else 'aresume.pdf')
+OUTPUT = os.path.expanduser(
+    conf["real_resume_install_path"] if BUILD_REAL else "aresume.pdf"
+)
 if BUILD_DIGITAL:
-    OUTPUT = OUTPUT[:-4] + '-digital' + OUTPUT[-4:]
-os.rename('texput.pdf', OUTPUT)
-print('built {}'.format(OUTPUT))
+    OUTPUT = OUTPUT[:-4] + "-digital" + OUTPUT[-4:]
+os.rename("texput.pdf", OUTPUT)
+print("built {}".format(OUTPUT))
