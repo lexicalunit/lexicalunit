e989915ad5238a5ac9c17afcab8f0b4c0352382e
lexicalunit <lexicalunit@lexicalunit.com>
2026-02-03 16:19:06 -0800

    Revamp main page a bit

--- www/js/lexicalunit.js
+++ www/js/lexicalunit.js
@@ -1,23 +1,37 @@
-var show_top = false
+var show_top = false;
 function toggle() {
-  show_top = !show_top
-  if(show_top) {
-    $('.top').addClass('invis')
-    $('.bottom').removeClass('invis')
-    $('.dryguy').attr('src', 'www/img/dryguy-stare.png')
-    $('.content').addClass('colorful')
+  show_top = !show_top;
+  if (show_top) {
+    $(".top").addClass("invis");
+    $(".bottom").removeClass("invis");
+    $(".dryguy").attr("src", "www/img/dryguy-stare.png");
+    $(".content").addClass("colorful");
+    // Show alt subtitle, hide resume
+    $(".subtitle").removeClass("invis");
+    $(".resume-link").addClass("invis");
   } else {
-    $('.top').removeClass('invis')
-    $('.bottom').addClass('invis')
-    $('.dryguy').attr('src', 'www/img/dryguy.gif')
-    $('.content').removeClass('colorful')
+    $(".top").removeClass("invis");
+    $(".bottom").addClass("invis");
+    $(".dryguy").attr("src", "www/img/dryguy.gif");
+    $(".content").removeClass("colorful");
+    // Hide alt subtitle, show resume
+    $(".subtitle").addClass("invis");
+    $(".resume-link").removeClass("invis");
   }
 }
 
+function toggleMenu() {
+  const menuPanel = document.querySelector(".menu-panel");
+  const hamburger = document.querySelector(".hamburger");
+
+  menuPanel.classList.toggle("open");
+  hamburger.classList.toggle("active");
+}
+
 function hover(element) {
-  element.classList.add('hovered')
+  element.classList.add("hovered");
 }
 
 function unhover(element) {
-  element.classList.remove('hovered')
+  element.classList.remove("hovered");
 }
